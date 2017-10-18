b28b2ed17781066ef71adb50a65ac9d3f0b81d7c
Oleksiy Chechel <alex.mirrr@gmail.com>
2017-10-18 16:11:30 +0300

    Add log output about engine running

--- engine.go
+++ engine.go
@@ -1,11 +1,13 @@
 package tokay
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"sort"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/night-codes/tokay-render"
 	"github.com/valyala/fasthttp"
@@ -56,104 +58,133 @@ var (
 
 // New creates a new Engine object.
 func New() *Engine {
-	r := &Engine{
+	engine := &Engine{
 		AppEngine: AppEngine,
 		routes:    make(map[string]*Route),
 		stores:    make(map[string]routeStore),
 	}
-	r.Render = render.New()
-	r.RouterGroup = *newRouteGroup("", r, make([]Handler, 0))
-	r.NotFound(MethodNotAllowedHandler, NotFoundHandler)
-	r.pool.New = func() interface{} {
+	engine.Render = render.New()
+	engine.RouterGroup = *newRouteGroup("", engine, make([]Handler, 0))
+	engine.NotFound(MethodNotAllowedHandler, NotFoundHandler)
+	engine.pool.New = func() interface{} {
 		return &Context{
-			pvalues: make([]string, r.maxParams),
-			engine:  r,
+			pvalues: make([]string, engine.maxParams),
+			engine:  engine,
 		}
 	}
-	return r
+	return engine
+}
+
+func runmsg(addr string, ec chan error, message string) (err error) {
+	if message != "" {
+		select {
+		case err = <-ec:
+			return
+		case _ = <-time.Tick(time.Second / 4):
+			if strings.Contains(message, "%s") {
+				fmt.Printf(message+"\n", addr)
+			} else {
+				fmt.Println(message)
+			}
+		}
+	}
+	err = <-ec
+	return
 }
 
 // Run attaches the engine to a fasthttp server and starts listening and serving HTTP requests.
 // It is a shortcut for fasthttp.ListenAndServe(addr, engine.HandleRequest) Note: this method will block the
 // calling goroutine indefinitely unless an error happens.
-func (r *Engine) Run(addr string) (err error) {
-	return fasthttp.ListenAndServe(addr, r.HandleRequest)
+func (engine *Engine) Run(addr string, message ...string) error {
+	ec := make(chan error)
+	go func() {
+		ec <- fasthttp.ListenAndServe(addr, engine.HandleRequest)
+	}()
+	return runmsg(addr, ec, append(message, "HTTP server started at %s")[0])
 }
 
 // RunTLS attaches the engine to a fasthttp server and starts listening and
 // serving HTTPS (secure) requests. It is a shortcut for
 // fasthttp.ListenAndServeTLS(addr, certFile, keyFile, engine.HandleRequest)
 // Note: this method will block the calling goroutine indefinitely unless an error happens.
-func (r *Engine) RunTLS(addr string, certFile, keyFile string) (err error) {
-	return fasthttp.ListenAndServeTLS(addr, certFile, keyFile, r.HandleRequest)
+func (engine *Engine) RunTLS(addr string, certFile, keyFile string, message ...string) error {
+	ec := make(chan error)
+	go func() {
+		ec <- fasthttp.ListenAndServeTLS(addr, certFile, keyFile, engine.HandleRequest)
+	}()
+	return runmsg(addr, ec, append(message, "HTTPS server started at %s")[0])
 }
 
 // RunUnix attaches the engine to a fasthttp server and starts listening and
 // serving HTTP requests through the specified unix socket (ie. a file).
 // Note: this method will block the calling goroutine indefinitely unless an error happens.
-func (r *Engine) RunUnix(addr string, mode os.FileMode) (err error) {
-	return fasthttp.ListenAndServeUNIX(addr, mode, r.HandleRequest)
+func (engine *Engine) RunUnix(addr string, mode os.FileMode, message ...string) error {
+	ec := make(chan error)
+	go func() {
+		ec <- fasthttp.ListenAndServeUNIX(addr, mode, engine.HandleRequest)
+	}()
+	return runmsg(addr, ec, append(message, "Unix server started at %s")[0])
 }
 
 // HandleRequest handles the HTTP request.
-func (r *Engine) HandleRequest(ctx *fasthttp.RequestCtx) {
-	c := r.pool.Get().(*Context)
+func (engine *Engine) HandleRequest(ctx *fasthttp.RequestCtx) {
+	c := engine.pool.Get().(*Context)
 	c.init(ctx)
-	c.handlers, c.pnames = r.find(string(ctx.Method()), string(ctx.Path()), c.pvalues)
+	c.handlers, c.pnames = engine.find(string(ctx.Method()), string(ctx.Path()), c.pvalues)
 	c.Next()
-	r.pool.Put(c)
+	engine.pool.Put(c)
 }
 
 // Route returns the named route.
 // Nil is returned if the named route cannot be found.
-func (r *Engine) Route(name string) *Route {
-	return r.routes[name]
+func (engine *Engine) Route(name string) *Route {
+	return engine.routes[name]
 }
 
 // Use appends the specified handlers to the engine and shares them with all routes.
-func (r *Engine) Use(handlers ...Handler) {
-	r.RouterGroup.Use(handlers...)
-	r.notFoundHandlers = combineHandlers(r.handlers, r.notFound)
+func (engine *Engine) Use(handlers ...Handler) {
+	engine.RouterGroup.Use(handlers...)
+	engine.notFoundHandlers = combineHandlers(engine.handlers, engine.notFound)
 }
 
 // NotFound specifies the handlers that should be invoked when the engine cannot find any route matching a request.
 // Note that the handlers registered via Use will be invoked first in this case.
-func (r *Engine) NotFound(handlers ...Handler) {
-	r.notFound = handlers
-	r.notFoundHandlers = combineHandlers(r.handlers, r.notFound)
+func (engine *Engine) NotFound(handlers ...Handler) {
+	engine.notFound = handlers
+	engine.notFoundHandlers = combineHandlers(engine.handlers, engine.notFound)
 }
 
 // handleError is the error handler for handling any unhandled errors.
-func (r *Engine) handleError(c *Context, err error) {
+func (engine *Engine) handleError(c *Context, err error) {
 	c.Error(err.Error(), http.StatusInternalServerError)
 }
 
-func (r *Engine) add(method, path string, handlers []Handler) {
-	store := r.stores[method]
+func (engine *Engine) add(method, path string, handlers []Handler) {
+	store := engine.stores[method]
 	if store == nil {
 		store = newStore()
-		r.stores[method] = store
+		engine.stores[method] = store
 	}
-	if n := store.Add(path, handlers); n > r.maxParams {
-		r.maxParams = n
+	if n := store.Add(path, handlers); n > engine.maxParams {
+		engine.maxParams = n
 	}
 }
 
-func (r *Engine) find(method, path string, pvalues []string) (handlers []Handler, pnames []string) {
+func (engine *Engine) find(method, path string, pvalues []string) (handlers []Handler, pnames []string) {
 	var hh interface{}
-	if store := r.stores[method]; store != nil {
+	if store := engine.stores[method]; store != nil {
 		hh, pnames = store.Get(path, pvalues)
 	}
 	if hh != nil {
 		return hh.([]Handler), pnames
 	}
-	return r.notFoundHandlers, pnames
+	return engine.notFoundHandlers, pnames
 }
 
-func (r *Engine) findAllowedMethods(path string) map[string]bool {
+func (engine *Engine) findAllowedMethods(path string) map[string]bool {
 	methods := make(map[string]bool)
-	pvalues := make([]string, r.maxParams)
-	for m, store := range r.stores {
+	pvalues := make([]string, engine.maxParams)
+	for m, store := range engine.stores {
 		if handlers, _ := store.Get(path, pvalues); handlers != nil {
 			methods[m] = true
 		}
